9e2a22c112a268de1c5d9b66fa453398b3b97ea9
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-02-28 22:25:56 +0530

    readme comments

--- Core Java/src/HelloWorld.java
+++ Core Java/src/HelloWorld.java
@@ -7,3 +7,30 @@ public class HelloWorld {
 	}
 
 }
+/*class :- It is keyword used declare a class
+ * public :- 
+ * 		Is an access modifier which represent visibility. public means it is visible to All
+ * static :-
+ * 		- It is also a keyword. If we declare any method as static, it is known as static method.
+ * 		- The core advantage of the static method is that there is no need to create an object to invoke the static method. 
+ * 		- The main method is executed by the JVM, so it doesn't require to create an object to invoke the main method. So it saves memor
+ * void :-
+ * 
+ * 		- It is the return type of the method.
+ * 		- It means it doesn't return any value.
+ * 
+ * main:-
+ * 		- represent the starting point of program.
+ * 		- It is also a keyword
+ * 
+ * String [] args :
+ * 
+ * 		- used for command line argument
+ * 
+ *  System.out.println()
+ *  	- used to print statement.
+ *  	- Here,
+ *  		1. System- is a class
+ *  		2. out -is the object of PrintStream class
+ * 			3. println -is a method of PrintStream class
+ * */
