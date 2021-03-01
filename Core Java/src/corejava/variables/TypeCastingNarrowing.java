4c30bcf4f21f3c576309f99d378fed04657121e5
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 15:13:47 +0530

    Narrowing

---
+++ Core Java/src/corejava/variables/TypeCastingNarrowing.java
@@ -0,0 +1,13 @@
+package corejava.variables;
+
+public class TypeCastingNarrowing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float f=23.8f;
+		int a=(int) f;
+		System.out.println("After Narrowing : "+a);
+
+	}
+
+}
