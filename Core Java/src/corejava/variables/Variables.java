6db594e5e96c5059e6f16ec8349506e6272f0e8a
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 14:54:41 +0530

    variables

---
+++ Core Java/src/corejava/variables/Variables.java
@@ -0,0 +1,16 @@
+package corejava.variables;
+class Sample{
+	
+	int data=50;//instance variable  
+	static int m=100;//static variable  
+	public void m1(){  
+	int n=90;//local variable  
+	}  
+}
+
+public class Variables {
+
+	Sample s1=new Sample();
+	
+	
+}
