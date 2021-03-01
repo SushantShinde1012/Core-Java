34acef999b8048c63a1e55b5a290e69794c0a5d5
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 16:58:31 +0530

    Positive Negative

---
+++ Core Java/src/corejava/controlstatement/PositiveNegative.java
@@ -0,0 +1,17 @@
+package corejava.controlstatement;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a=0;
+			if(a>0) {
+				System.out.println("Number is positive");
+			}else if(a < 0) {
+				System.out.println("Number is negative");
+			}else {
+				System.out.println("Number is zero");
+			}
+	}
+
+}
