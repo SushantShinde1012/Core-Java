80e6fd33f2f9300f6a919b6837387ab4d32997dc
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 15:00:38 +0530

    simple Addition

---
+++ Core Java/src/corejava/variables/Addition.java
@@ -0,0 +1,15 @@
+package corejava.variables;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		int b=23;
+		
+		int c= a+b;
+		System.out.println("Addition of : "+a+"  And  "+b+"  is  "+c);
+	}
+
+}
