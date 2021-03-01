53dd308b35e61b2bb0f83752b215dd0b8c0cf7e3
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 16:21:44 +0530

    if-else demo

---
+++ Core Java/src/corejava/controlstatement/OddEven.java
@@ -0,0 +1,15 @@
+package corejava.controlstatement;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=11;
+		if(a%2==0) {
+			System.out.println("Number is Even");
+		}else {
+			System.out.println("Number is Odd");
+		}
+	}
+
+}
