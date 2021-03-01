b8305a16c9f32efffbb9ea5af024e5e093f14f36
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 15:53:42 +0530

    some additional

--- Core Java/src/corejava/operators/UnaryOperators.java
+++ Core Java/src/corejava/operators/UnaryOperators.java
@@ -5,10 +5,17 @@ public class UnaryOperators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
+		int b=-12;
+		boolean c= true;
+		boolean d=false;
+		
 		System.out.println(a++);
 		System.out.println(++a);
 		System.out.println(a--);
 		System.out.println(--a);
+		System.out.println(~a);//-11 (minus of total positive value which starts from 0)  
+		System.out.println(~b);////11 (positive of total minus, positive starts from 0)  
+		System.out.println(!c);//false (opposite of boolean value)  
 	}
 
 }
