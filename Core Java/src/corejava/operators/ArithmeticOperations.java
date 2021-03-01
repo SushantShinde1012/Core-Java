0af05d768481186ff35b5e0597f79ba85f5d47f0
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 15:58:49 +0530

    Arithmetic Operations

---
+++ Core Java/src/corejava/operators/ArithmeticOperations.java
@@ -0,0 +1,15 @@
+package corejava.operators;
+
+public class ArithmeticOperations {
+	
+	public static void main(String[] args) {
+		
+		int a=23;
+		int b=17;
+		System.out.println("Addition : "+(a+b));
+		System.out.println("Substraction : "+(a-b));
+		System.out.println("Multiplication : "+(a*b));
+		System.out.println("Division : "+(a/b));
+		System.out.println("Modulus : "+(a%b));
+	}
+}
