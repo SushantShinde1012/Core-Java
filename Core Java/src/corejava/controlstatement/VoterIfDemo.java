40562fd9ef56adbbe9f4da2ada3a30eaa7365d56
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 16:19:05 +0530

    If Demo

---
+++ Core Java/src/corejava/controlstatement/VoterIfDemo.java
@@ -0,0 +1,13 @@
+package corejava.controlstatement;
+
+public class VoterIfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int age=20;
+			if(age>=18) {
+				System.out.println("You can Vote");
+			}
+	}
+
+}
