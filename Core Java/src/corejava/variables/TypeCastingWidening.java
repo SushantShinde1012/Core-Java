4b887c0c404cb8d8b2c858ea939e3908e8f62737
SushantShinde1012 <77279055+SushantShinde1012@users.noreply.github.com>
2021-03-01 15:11:51 +0530

    Widening

---
+++ Core Java/src/corejava/variables/TypeCastingWidening.java
@@ -0,0 +1,13 @@
+package corejava.variables;
+
+public class TypeCastingWidening {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a=12;
+			float f=a;
+			System.out.println("Before Widening a: "+a);
+			System.out.println("After Widening to float  f: "+f);
+	}
+
+}
