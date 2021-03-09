4ab825b21b7d87e73547c57fde013abc59df2cc0
shindesush <sss917023@INBR2WKS-195480.tatatechnologies.com>
2021-03-09 10:44:50 +0530

    casting

--- Java Basics/src/com/core/FirstCode.java
+++ Java Basics/src/com/core/FirstCode.java
@@ -9,12 +9,27 @@ public class FirstCode {
 		// Variables
 		
 		int a = 23; // 4 bytes
-		double percent =63.50;
-		float percent1 = 63.50f;
+		double percent =63.50;//8 bytes
+		float percent1 = 63.50f;// 4bytes
+		long l =50000000l;//8 bytes
+		char c='A';
+		
+		
+		
 		
 		System.out.println("Value of a : "+a);
 		System.out.println("Double value : "+percent);
 		System.out.println("Float Value : "+percent1);
+		
+		double d1=5;// Implicit Type Casting
+		int k =(int)5.7;// Explicit type casting
+		
+		/*
+		 * byte -->short --> int --> long --> float --> double
+		 * 
+		 * 
+		 * */
+		System.out.println("Value of K  : "+k);
 	}
 
 }
