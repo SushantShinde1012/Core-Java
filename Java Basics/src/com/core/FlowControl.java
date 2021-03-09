0721a29fea8fb71c08b7e57cfefffdb598cc4b9a
shindesush <sss917023@INBR2WKS-195480.tatatechnologies.com>
2021-03-09 11:26:19 +0530

    if else

---
+++ Java Basics/src/com/core/FlowControl.java
@@ -0,0 +1,77 @@
+package com.core;
+
+/*Flow Control
+ *  - Describes the order , in which statements will be executed at run time
+ *  - There are three types
+ * 
+ * 		1. Selection Statements
+ * 
+ * 			- Several options are there among that one is executed.
+ * 				a. if - else
+ * 					
+ * 					- syntax
+ * 
+ * 						if(boolean condition b){
+ * 							Action if b is true
+ * 						}
+ * 						else{
+ * 							Action if b is false
+ * 						}
+ * 
+ * 					- argument to if statement must be boolean type, if we are trying to provide any other type then we will get compile time error.
+ * 
+ * 
+ * 				b. switch()
+ * 
+ * 		2. Iterative Statements
+ * 			- Group of statement is executed iteratively
+ * 				a. while loop
+ * 				b. do-- while()
+ * 				c. for loop
+ * 				d. for each loop
+ * 		
+ * 		3. Control Transfer statement
+ * 			- To transfer the control to one place to another
+ *  			a. break
+ *  			b. continue
+ *  			c. return
+ *  			d. try catch finally
+ *  			e. assert
+ * 
+ * 
+ * */
+public class FlowControl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// if possibilities
+		
+	/*
+		int x= 0;
+		if(x) {
+			System.out.println("Hello");
+		}else {
+			System.out.println("tets");
+		}
+		 - It gives compile time error as if only takes boolean
+		 error : Type mismatch: cannot convert from int to boolean
+	*/
+		int x=10;
+		if(x==20) {
+			System.out.println("Hello");
+		}else {
+			System.out.println("Hi");
+		}
+		
+		
+		boolean b=true;
+		if(b=false) {
+			System.out.println("Hello ");
+		}else {
+			System.out.println("Hi");
+		}
+		
+		
+	}
+
+}
