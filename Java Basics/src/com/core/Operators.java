817e0372250fa3b02d060b0d61e8a4f457490109
shindesush <sss917023@INBR2WKS-195480.tatatechnologies.com>
2021-03-09 11:12:31 +0530

    operators

--- Java Basics/src/com/core/Operators.java
+++ Java Basics/src/com/core/Operators.java
@@ -23,6 +23,10 @@ public class Operators {
 			int m=78, n=34;
 			System.out.println("Addition : "+(m+n)+"\n Substraction : "+(m-n)+"\n Multiplication "+(m*n)+"\n Division "+(m/n)+"\n Mudulus Division "+(m%n));
 			
+			// Short hand Opertaor
+			// n=n+m; instead
+			n+=m;
+			System.out.println(n);
 	}
 
 }
