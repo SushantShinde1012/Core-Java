d80a3207dce6d055389d2822e47af79b19cc31bf
shindesush <sss917023@INBR2WKS-195480.tatatechnologies.com>
2021-03-09 10:37:44 +0530

    var

--- Java Basics/src/com/core/FirstCode.java
+++ Java Basics/src/com/core/FirstCode.java
@@ -5,7 +5,16 @@ public class FirstCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World....!");
-
+		
+		// Variables
+		
+		int a = 23; // 4 bytes
+		double percent =63.50;
+		float percent1 = 63.50f;
+		
+		System.out.println("Value of a : "+a);
+		System.out.println("Double value : "+percent);
+		System.out.println("Float Value : "+percent1);
 	}
 
 }
