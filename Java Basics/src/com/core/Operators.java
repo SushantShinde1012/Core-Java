1e1f5a54509d2d2b70db389664d6a17c017ff4b9
shindesush <sss917023@INBR2WKS-195480.tatatechnologies.com>
2021-03-09 10:55:47 +0530

    Arithmetic

---
+++ Java Basics/src/com/core/Operators.java
@@ -0,0 +1,28 @@
+package com.core;
+
+public class Operators {
+
+	
+	
+		/*
+		 * Arithmetic
+		 * 		-> + , -, *, / , %		
+		 * 
+		 * 
+		 * Bitwise
+		 * 
+		 * Relational
+		 * 
+		 * Logical
+		 * 
+		 * 
+		 * */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			int m=78, n=34;
+			System.out.println("Addition : "+(m+n)+"\n Substraction : "+(m-n)+"\n Multiplication "+(m*n)+"\n Division "+(m/n)+"\n Mudulus Division "+(m%n));
+			
+	}
+
+}
